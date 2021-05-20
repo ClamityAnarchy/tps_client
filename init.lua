f16de0cb14f6a4269767e6cee4d3423b4d01e0b0
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-05-20 13:06:56 +0200

    Add ping hud that shows the elapsed time since the server has sent the last TPS command in milliseconds

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 minetest.mod_channel_join("tps")
 
-local hud
+local hud, ping_hud, ping
 
 minetest.register_on_modchannel_message(function(channel_name, sender, message)
 	if sender == "" and channel_name == "tps" and minetest.localplayer then
@@ -15,6 +15,22 @@ minetest.register_on_modchannel_message(function(channel_name, sender, message)
 				text = message,
 				number = 0xFFFFFF,
 			})
+			ping_hud = minetest.localplayer:hud_add({
+				hud_elem_type = "text",
+				position = {x = 1, y = 1},
+				alignment = {x = -1, y = -1},
+				offset = {x = -50, y = -25},
+				text = "0",
+				number = 0xFFF800,
+			})
 		end
+		ping = 0
+	end
+end)
+
+minetest.register_globalstep(function(dtime)
+	if ping then
+		ping = ping + dtime
+		minetest.localplayer:hud_change(ping_hud, "text", tostring(math.floor(ping * 1000)))
 	end
 end)
